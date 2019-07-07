e065f595859ae6df0a58ac3c353b0cd6f6579fc3
suhailvs <suhailvs@gmail.com>
2019-07-07 09:25:09 +0530

    added LDClerk 01-07-17 questions

--- sampledata/import_questions.py
+++ sampledata/import_questions.py
@@ -2,19 +2,19 @@
 Usage 
 ./manage.py shell
 >>> from sampledata import import_questions
->>> import_questions.sync('30-1-10')
+>>> import_questions.sync('LD_Clerk','01-01-98')
 """
 
-from classroom.models import Quiz, Question, Answer
+from classroom.models import Quiz, Question, Answer, Subject
 import os
 
-def sync(book):
-    quiz, created = Quiz.objects.get_or_create(owner_id = 2, name='PSC {0}'.format(book), subject_id=5)
-    # created = True
+def sync(subject, exam_date):
+    item, _ = Subject.objects.get_or_create(name = subject.replace('_',' '))
+    quiz, created = Quiz.objects.get_or_create(owner_id = 2, name=exam_date, subject=item)
     if not created:
         return 'quiz already exist'
 
-    filename = os.path.join('sampledata', '{0}.txt'.format(book))
+    filename = os.path.join('sampledata', subject, '{0}.txt'.format(exam_date))
     fp = open(filename,'r')
     for line in fp.readlines():
         items = line.split(',')
