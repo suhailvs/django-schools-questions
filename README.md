2cb2c1d2a2200517848a53e78d68ef9e8c47269e
suhailvs <suhailvs@gmail.com>
2019-07-14 13:29:11 +0530

    added loop through directory to find files for sync

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # django-schools-questions
+
+## Usage 
+
+Copy `sampledata` folder to project folder
+::
+
+    $ ./manage.py shell
+    >>> from sampledata import import_questions
+    >>> import_questions.sync()
--- sampledata/import_questions.py
+++ sampledata/import_questions.py
@@ -1,14 +1,19 @@
 """
 Usage 
-./manage.py shell
->>> from sampledata import import_questions
->>> import_questions.sync('LD_Clerk','01-01-98')
+=====
+Copy sampledata folder to project folder
+::
+
+    $ ./manage.py shell
+    >>> from sampledata import import_questions
+    >>> import_questions.sync()
 """
 
 from classroom.models import Quiz, Question, Answer, Subject
 import os
 
-def sync(subject, exam_date):
+def sync_exam(subject, exam_date):
+
     item, _ = Subject.objects.get_or_create(name = subject.replace('_',' '))
     quiz, created = Quiz.objects.get_or_create(owner_id = 2, name=exam_date, subject=item)
     if not created:
@@ -24,3 +29,12 @@ def sync(subject, exam_date):
             correct = int(items[-1]) == (i+1)
             answer = Answer.objects.create(question = question, text = ans.strip(), is_correct=correct)
             print (answer)
+
+def sync():
+    # r=root, d=directories, f = files
+    for r, d, f in os.walk('sampledata'):
+        for file in f:
+            if ".txt" in file:
+                subject = r.split('/')[1]
+                exam_date = file.split('.')[0]
+                sync_exam(subject,exam_date)
